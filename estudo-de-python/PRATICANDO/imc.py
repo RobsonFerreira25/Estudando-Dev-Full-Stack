845af0f81cbedb0688f7a188439d8f68758a7cf3
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-09-24 07:11:01 -0300

    add classifica imc

--- estudo-de-python/PRATICANDO/imc.py
+++ estudo-de-python/PRATICANDO/imc.py
@@ -5,3 +5,14 @@ def calcula_imc(peso, altura):
     print('Parâmetro altura:', altura)
     imc = peso / altura **2
     return imc
+
+def classifica_imc(indice):
+    if indice < 18.5:
+        return 'Baixo peso'
+    elif indice < 25:
+        return 'Peso adequado'
+    elif indice < 30:
+        return 'Sobrepeso'
+    else:
+        return 'Obeso'
+    
---
+++ estudo-de-python/PRATICANDO/principal.py
@@ -0,0 +1,1 @@
+from imc import calcula_imc
