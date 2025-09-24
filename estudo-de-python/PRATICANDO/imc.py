e251aa9b330d66374703ec1fc97219c0ea5442ff
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-09-24 06:38:51 -0300

    modulo imc

---
+++ estudo-de-python/PRATICANDO/imc.py
@@ -0,0 +1,7 @@
+print('Módulo IMC importado')
+
+def calcula_imc(peso, altura):
+    print('Parâmetro peso:', peso)
+    print('Parâmetro altura:', altura)
+    imc = peso / altura **2
+    return imc
