1a55467017c4ab9467079a0581e181a77d2a04e7
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-09-26 12:06:15 -0300

    botao

---
+++ estudo-de-python/PRATICANDO/tkinter_pratic/botao.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import Button
+
+principal = tkinter.Tk()
+# O codigo vai aqui
+
+botao = Button(principal, text='Bem vindo Robson')
+botao.place(x=100, y=100)
+
+principal.mainloop()
