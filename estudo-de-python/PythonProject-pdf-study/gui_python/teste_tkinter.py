7850341762461eba47f9f1fa402e679db524d76d
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-10-23 07:14:39 -0300

    teste flexx

---
+++ estudo-de-python/PythonProject-pdf-study/gui_python/teste_tkinter.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Teste Tkinter")
+
+button1 = tk.Button(root, text="Olá")
+button1.pack() 
+button2 = tk.Button(root, text="Mundo")
+button2.pack()
+
+root.mainloop()
+
+
