209ae656738fe20b46596cb164d48e891cd6a3f4
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-10-27 07:45:22 -0300

    widget Combox

---
+++ estudo-de-python/PythonProject-pdf-study/gui_python/widget_Combobox.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+janela = tk.Tk()
+janela.title("Combobox")
+janela.geometry("500x250")
+
+# Componemte Label
+ttk.Label(janela, text="Combobox Widget", background="green", foreground="white", font=("Times New Roman", 15)).grid(row=0, column=1)
+ttk.Label(janela, text="Selecione um mês:", font=("Times New Roman", 10)).grid(column=0, row=5, padx=10, pady=5)
+
+# Componente Combobox
+n = tk.StringVar()
+escolha = ttk.Combobox(janela, width=27, textvariable=n)
+
+# Adicção de itens no Combobox
+escolha["values"] = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+escolha.grid(column=1, row=5)
+escolha.current()
+
+janela.mainloop()
