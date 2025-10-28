26f263c250e3e140677e9147c613d1f0345ff5cd
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-10-28 06:36:49 -0300

    updat inserção de dados

--- estudo-de-python/PythonProject-pdf-study/gui_banco_dados/inserindo_dados_tabela.py
+++ estudo-de-python/PythonProject-pdf-study/gui_banco_dados/inserindo_dados_tabela.py
@@ -10,7 +10,7 @@ conn = psycopg2.connect(
 print("Conexão com o banco de dados aberta com sucesso!")
 
 cur = conn.cursor()
-cur.execute('''INSERT INTO public.AGENDA (id, nome, telefone) VALUES (1, 'Pessoa 1', '011954542525')''')
+cur.execute('''INSERT INTO public.AGENDA (id, nome, telefone) VALUES (2, 'Pessoa 2', '011954542545')''')
 
 conn.commit()
 print("Inserção realizada com sucesso!"); 
