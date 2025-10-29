fc40fac7a05ee3d68a501ef7156437a23955a97b
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-10-29 06:56:12 -0300

    funcao conectar livrosdb

---
+++ estudo-de-python/gui_banco_dados/App_livros/db_config.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+def conectar():
+    try:
+        conexao = psycopg2.connect(
+            database="livrosdb",
+            user="postgres",
+            password="488981",
+            host="127.0.0.1",
+            port="5432"
+        )
+        return conexao
+    except Exception as e:
+        print("Erro ao conectar ao banco de dados", e)
+        return None
+    
