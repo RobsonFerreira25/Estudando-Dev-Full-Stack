3a59f2576e4252b24742cb3a4a6b6dfb8ebb814f
Robson Ferreira <robsondonasferreira@hotmail.com>
2025-11-23 12:09:06 -0300

    Update ex001-pdf-study.py

--- estudo-de-python/PRATICANDO/ex001-pdf-study.py
+++ estudo-de-python/PRATICANDO/ex001-pdf-study.py
@@ -1,1 +1,26 @@
 print('Robson - 488981 - FS2025 - 2025')
+distancia = 350
+combustivel = 28
+
+consumo = distancia / combustivel
+print(f"O consumo total e de: {consumo}")
+
+def consumo_medio(distancia, combustivel):
+    consumo = distancia / combustivel
+    print(f"O consumo medio é: {consumo}")
+    
+    
+    
+meu_consumo = consumo_medio(500, 10)
+
+
+
+reais = 500
+cotacao = 5.25
+dolares = reais / cotacao
+print(f"R$ {reais:.2f} = US$ {dolares:.2f}")
+
+
+salario_bruto = 2.800,00
+inss = 8%
+ir = 
